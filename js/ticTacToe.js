8133a1c616b9499e1c239688ce68aabb4b9df958
= <=>
2021-10-22 15:21:36 -0400

    Add function to display board

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,11 +1,38 @@
 const gameBoard = (() => {
-  const board = ['','','','','','','','','']
+  const board = ['', '', '', '', '', '', '', '', '']
+  
+  const display = () => {
+    let boardElem = document.createElement('div')
+    boardElem.classList.add('board')
+    for (let i = 0; i < board.length; i++) {
+      let squareElem = document.createElement('div')
+      squareElem.classList.add('square')
+      boardElem.appendChild(squareElem)
+    }
+    return document.querySelector('main').appendChild(boardElem)
+  }
+
+  return {display}
 })()
 
-const gameController = (() => {
+const gameLoop = (() => {
   
 })()
 
-const Player = (name, piece) => {
-  return {name, piece}
+const Player = (name) => {
+  return {name}
 }
+
+const playerOne = Player('P1')
+const playerTwo = Player('P2')
+
+const SQUARES = document.querySelectorAll('.square')
+
+SQUARES.forEach((square, i) => {
+  square.addEventListener('click', () => {
+    console.log({ i, square })
+    square.innerHTML = 'X'
+  })
+})
+
+gameBoard.display()
