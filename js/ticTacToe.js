0d56d6f93228ed2520b1eb6fde03286640a51f18
= <=>
2021-10-23 16:01:41 -0400

    Update pieces to svg

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,14 +1,22 @@
 const Player = (name, piece) => {
-  return {name, piece}
+  let icon
+  if (piece == "X") {
+    icon = `
+    <svg width="50px" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark" class="svg-inline--fa fa-xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"></path></svg>`
+  } else if(piece == 'O'){
+    icon = `
+    <svg width="55px" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="o" class="svg-inline--fa fa-o" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224 32.01c-123.5 0-224 100.5-224 224s100.5 224 224 224s224-100.5 224-224S347.5 32.01 224 32.01zM224 416c-88.22 0-160-71.78-160-160s71.78-159.1 160-159.1s160 71.78 160 159.1S312.2 416 224 416z"></path></svg>`
+  }
+  return {name, piece, icon}
 }
 
 // Update Board, track moves, prevent replacement
 const board = (() => {
   const memory = new Array(9).fill('')
 
-  const update = (target, i, piece) => {
+  const update = (target, i, piece, icon) => {
     memory[i] = piece 
-    target.innerText = piece
+    target.innerHTML = icon
   }
 
   return {
@@ -51,13 +59,15 @@ const controller = (() => {
   }
 
   elem_board.addEventListener('click', (e) => {
+    console.log(e.target)
     let targetElem = e.target
     let targetIndex = targetElem.dataset.index
     let currentPiece = currentPlayer.piece
+    let currentIcon = currentPlayer.icon
     let winner = false
     
     if (!board.memory[targetIndex]) {
-      board.update(targetElem, targetIndex, currentPiece)
+      board.update(targetElem, targetIndex, currentPiece, currentIcon)
       if (isWin(currentPiece)) {
         winner = true
         console.log(`${currentPlayer.name} Wins!`)
