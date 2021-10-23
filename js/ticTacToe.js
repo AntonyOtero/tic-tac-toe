1ebd47d21bbd0e6753dbf19735b7d0b0e29d2ca2
= <=>
2021-10-23 10:52:01 -0400

    Update - complete refactor
    - The board is now generated through markup instead of
      JS since the board itself never needs to change just
      the content.
    - Refactored JS to use a Player factory to generate the
      two Player objects
    - refactored JS to utilize modules to generate a board
      object and Controller object.
    - The Event Listner is now attached to the div.board
      element, allowing access to each tile via the Event
      objects target property, instead of having to loop
      through the div. board element and add an Event
      listner to each individual tile.

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,38 +1,78 @@
-const gameBoard = (() => {
-  const board = ['', '', '', '', '', '', '', '', '']
-  
-  const display = () => {
-    let boardElem = document.createElement('div')
-    boardElem.classList.add('board')
-    for (let i = 0; i < board.length; i++) {
-      let squareElem = document.createElement('div')
-      squareElem.classList.add('square')
-      boardElem.appendChild(squareElem)
-    }
-    return document.querySelector('main').appendChild(boardElem)
-  }
-
-  return {display}
-})()
-
-const gameLoop = (() => {
-  
-})()
-
-const Player = (name) => {
-  return {name}
+const Player = (name, piece) => {
+  return {name, piece}
 }
 
-const playerOne = Player('P1')
-const playerTwo = Player('P2')
+// Update Board, track moves, prevent replacement
+const board = (() => {
+  const memory = new Array(9).fill('')
 
-const SQUARES = document.querySelectorAll('.square')
+  const update = (target, i, piece) => {
+    memory[i] = piece 
+    target.innerText = piece
+  }
 
-SQUARES.forEach((square, i) => {
-  square.addEventListener('click', () => {
-    console.log({ i, square })
-    square.innerHTML = 'X'
+  return {
+    memory,
+    update
+  }
+})()
+
+// Control game logic
+// turns, wins/draws
+const controller = (() => {
+  const elem_board = document.querySelector('.board')
+  const playerOne = Player('p1', 'X')
+  const playerTwo = Player('p2', 'O')
+  let currentPlayer = playerOne
+
+  const isWin = (piece) => {
+    let winConditions = [
+      [board.memory[0], board.memory[1], board.memory[2]],
+      [board.memory[0], board.memory[4], board.memory[8]],
+      [board.memory[0], board.memory[3], board.memory[6]],
+      [board.memory[1], board.memory[4], board.memory[7]],
+      [board.memory[3], board.memory[4], board.memory[5]],
+      [board.memory[6], board.memory[4], board.memory[2]],
+      [board.memory[2], board.memory[5], board.memory[8]],
+      [board.memory[6], board.memory[7], board.memory[8]],
+    ]
+
+    return winConditions
+      .map((arr) => {
+        return arr
+          .map(value => value == piece)
+          .reduce((preBool, curBool) => preBool && curBool)
+      })
+      .reduce((preBool, curBool) => preBool || curBool)
+  }
+
+  const isDraw = (isWinner) => {
+    return !isWinner && board.memory.reduce((preTile, curTile) => Boolean(preTile) && Boolean(curTile))
+  }
+
+  elem_board.addEventListener('click', (e) => {
+    let targetElem = e.target
+    let targetIndex = targetElem.dataset.index
+    let currentPiece = currentPlayer.piece
+    let winner = false
+    
+    if (!board.memory[targetIndex]) {
+      board.update(targetElem, targetIndex, currentPiece)
+      if (isWin(currentPiece)) {
+        winner = true
+        console.log(`${currentPlayer.name} Wins!`)
+      } else if (isDraw(winner)) {
+        console.log(`It's a draw!`)
+      } else {
+        currentPlayer = (currentPlayer.name == 'p1')
+          ? playerTwo
+          : playerOne
+      }
+    }
   })
-})
 
-gameBoard.display()
+  return {
+    playerOne,
+    playerTwo
+  }
+})()
